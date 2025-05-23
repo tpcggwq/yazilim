6c59b5705f5ea97afdc4803be8bffecf0327f65e
tpcggwq <kayuaben@gmail.com>
2025-05-23 07:32:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 
-from config import Config
- app.config.from_object(Config)
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
